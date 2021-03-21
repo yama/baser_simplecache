db2d8560fa715e064b4395bdaeff8eef5cc109a0
Yamamoto <yamamoto@catchup.co.jp>
2021-03-21 14:30:43 +0900

    update

--- SimpleCache/Config/bootstrap.php
+++ SimpleCache/Config/bootstrap.php
@@ -1,26 +1,4 @@
 <?php
-/**
- * This file is loaded automatically by the app/webroot/index.php file after core.php
- *
- * This file should load/create any application wide configuration settings, such as
- * Caching, Logging, loading additional configuration files.
- *
- * You should also use this file to include any files that provide global functions/constants
- * that your application uses.
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @package       app.Config
- * @since         CakePHP(tm) v 0.10.8.2117
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */
 
 if(!defined('SimpleCacheInstalled') && is_file(WWW_ROOT . 'index.php')) {
 	file_put_contents(
