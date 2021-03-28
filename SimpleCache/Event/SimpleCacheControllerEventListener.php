d59b7c317ae1a63dc93e32478f8524a919897bda
yama <yamamoto@kyms.jp>
2021-03-29 06:59:56 +0900

    update

---
+++ SimpleCache/Event/SimpleCacheControllerEventListener.php
@@ -0,0 +1,16 @@
+<?php
+class SimpleCacheControllerEventListener extends BcControllerEventListener {
+    public $events = [
+        'initialize'
+    ];
+    public function initialize(CakeEvent $event) {
+		if (BcUtil::loginUserName()) {
+			if(empty($_COOKIE['BASER_LOGGED_IN'])) {
+				// todo できればキー末尾にハッシュ文字列をつけたい
+				setcookie('BASER_LOGGED_IN', 'loggedin', time()+60*60*24*180, baseUrl());
+			}
+		} elseif(!empty($_COOKIE['BASER_LOGGED_IN'])) {
+			setcookie('BASER_LOGGED_IN', '', time()-3600, baseUrl());
+		}
+    }
+}
